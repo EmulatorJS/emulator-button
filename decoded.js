2ebb163f9e83fdcdbe945aa0e6051a4d00c7656b
Ethan O'Brien <ethan.a.obrien@gmail.com>
2021-11-29 09:04:32 -0600

    Fix

--- decoded.js
+++ decoded.js
@@ -180,6 +180,7 @@
         if (! cachedFilesVersion) {
             var forceUpdate = true;
             status.innerHTML = 'Offline Mode: DOWNLOADING FILES';
+            localStorage.setItem('commonModulesCacheVersion', window.versionJSON.jsFilesVersion);
         } else if (window.versionJSON && window.versionJSON.jsFilesVersion && cachedFilesVersion < window.versionJSON.jsFilesVersion) {
             var forceUpdate = true;
             status.innerHTML = 'Offline Mode: UPDATING FILES';
